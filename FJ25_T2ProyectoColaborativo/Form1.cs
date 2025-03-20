fcd50570b0a897ebbde3dae70209628efe44398b
Eric Escobar Mendoza <eescobar@cdhidalgo.tecnm.mx>
2025-03-20 09:51:27 -0600

    Se agrago un mensaje en el boton

--- FJ25_T2ProyectoColaborativo/Form1.cs
+++ FJ25_T2ProyectoColaborativo/Form1.cs
@@ -16,5 +16,10 @@ namespace FJ25_T2ProyectoColaborativo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
